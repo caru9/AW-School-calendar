7214571c3542e518ef00b3fcc1e2843c591dda20
caru9 <69743685+caru9@users.noreply.github.com>
2020-08-16 07:50:37 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -69,56 +69,17 @@ router.post('/home/abs/displayabs',[session.check_loggedin,abscontroller.retriev
 
 ////--------------------------- API Routes ----------------------------// 
 
-// require controllers module 
-// var auth = require('../config/session'); 
-//var apiusercontroller = require('../controllers/apiusercontroller');
 var apilessoncontroller =require('../controllers/apilessoncontroller');
-//var apiclustercontroller = require('../controllers/apiclustercontroller'); 
-//var apiabscontroller =require('../controllers/apiabscontroller');
-//var logger = require('../config/session'); 
-
-
-// ----------------------------------USER Routes--------------------// 
-// for login & register
-//router.get('/api/login',apiusercontroller.viewlogin); //not necessary ? 
-//router.get('/register', usercontroller.viewregister); 
-//router.post('/register', usercontroller.register); 
-
-//router.post('/auth',[usercontroller.checklog,session.check_loggedin, usercontroller.tohome]); 
-
-//for logout 
-//router.get('/logout',usercontroller.userlogout);
-
-// for homepage
-//router.get('/home',[session.check_loggedin,usercontroller.viewhomepage]);
-//add middelware if own module session 
-//logger.logger_info
 
 //------------------------- LESSON ROUTE -----------------------//
 router.get('/api/lesson',[apilessoncontroller.viewlesson]);
-//session.idcurrent
-//router.post('/home/lesson/addlesson',[session.check_loggedin,lessoncontroller.createlesson]); 
-//session.idcurrent
-//router.post('/home/lesson/deletelesson/:lessonid',[session.check_loggedin,lessoncontroller.deletelesson]); 
-//router.post('/home/lesson/alterlesson/:lessonid',[session.check_loggedin,lessoncontroller.alterlesson]); 
-//router.post('/home/lesson/updatelesson/:lessonid',lessoncontroller.updatelesson); 
+router.post('/home/lesson/addlesson',apilessoncontroller.createlesson); 
+router.post('/home/lesson/deletelesson/:lessonid',[session.check_loggedin,lessoncontroller.deletelesson]); 
+router.post('/home/lesson/alterlesson/:lessonid',[session.check_loggedin,lessoncontroller.alterlesson]); 
+router.post('/home/lesson/updatelesson/:lessonid',lessoncontroller.updatelesson); 
 
 
-//---------------------- CLUSTER (GROUP & STUDENT) ROUTES ------------// 
-//router.get('/home/cluster',[session.check_loggedin,clustercontroller.viewcluster]); 
-//router.post('/home/cluster/addgroup',[session.check_loggedin,clustercontroller.creategroup]); 
-//router.post('/home/cluster/addstudent',[session.check_loggedin,clustercontroller.createstudent]); 
-//router.post('/home/cluster/deletegroup/:idclass',[session.check_loggedin,clustercontroller.deletegroup]); 
-//router.post('/home/cluster/deletestudent/:idstudent',[session.check_loggedin,clustercontroller.deletestudent]); 
 
-
-// --------------------- ABS routes ---------------// 
-//router.get('/home/abs',[session.check_loggedin, abscontroller.viewabs]); 
-//router.post('/home/abs/logabs',[session.check_loggedin,abscontroller.createabs]);
-//router.post('/home/abs/displayabs',[session.check_loggedin,abscontroller.retrieveabs,abscontroller.display]); 
-
-
-//exporting module
 module.exports =router; 
 
 
