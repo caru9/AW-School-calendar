5c7e0f253b56bf60bdeefa0b9e29463dd16fed53
caru9 <69743685+caru9@users.noreply.github.com>
2020-08-16 07:49:43 +0200

    Update apilessoncontroller.js

--- controllers/apilessoncontroller.js
+++ controllers/apilessoncontroller.js
@@ -1,15 +1,47 @@
 var Lesson = require('../models/lessonmodel'); 
 var Group = require('../models/groupmodel'); 
 
-// view - get lesson page populated 
+
 exports.viewlesson = function(req,res){
-        userid = req.session.userid; 
-        userid=1; 
-    Lesson.display(userid, function(data){
-        let lessonlist = data;  
-            res.json({lessonlist:lessonlist});
-    });  
+    userid = 1 
+Lesson.display(userid, function(data){
+    let lessonlist = data;  
+    Group.display_form(function(data2){
+        let uniquegroup = data2; 
+        res.json({lessonlist:lessonlist,uniquegroup:uniquegroup});
+    })
+
+});  
 }; 
 
+exports.createlesson = function(req,res) {
+    var weekday = req.body.weekday; 
+    var hour = req.body.hour; 
+    var teacher_id = 1
+    var class_name = req.body.class_name; 
+    var newlesson = new Lesson (weekday,hour,teacher_id,class_name); 
+    Lesson.create(newlesson,function(err){
+        if(err) {
+            res.status(404).json({'message':error}); 
+        } else {
+            res.status(200).json({'message':"ALL gooad"}); 
+      
+        }
+    }); 
+}; 
+
+// delete selected lesson
+exports.deletelesson = function(req,res) {
+    var idlesson = req.params.lessonid;
+    console.log(idlesson); 
+    Lesson.delete(idlesson, function(err) {
+        if(err) {
+            res.status(404).json({'message':error}); 
+        } else {
+            res.status(200).json({'message':"ALL gooad"}); 
+      
+        }
+    }); 
+}; 
 
 
