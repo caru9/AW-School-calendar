ddae65cc88862b0b31cc6306189e7d875a1da0b1
caru9 <69743685+caru9@users.noreply.github.com>
2020-08-28 12:03:49 +0200

    Update apiusercontroller.js

--- controllers/apiusercontroller.js
+++ controllers/apiusercontroller.js
@@ -9,7 +9,8 @@ exports.checklog = function(req,res,err) {
     var password = req.body.password; 
     User.check_log(email,password, function(data){
         if (data.length > 0) {
-                res.json({'data':'data'});  
+            req.session.userid = data[0].idteacher; 
+            res.json({'data':data});    
         } else {
             res.status(404).json({message:err}); 
         }
@@ -26,7 +27,7 @@ exports.register = function(req, res) {
             if(err) {
                 res.status(404).json({'message':err}); 
             } else {
-                res.status(200); 
+                res.status(200).json({'message':'user recorded'}); 
                 console.log('user added'); 
             }   
     });  
